8a813158ad6b17479e866459dfb5c208d35689e3
zlbr <mingrn97@gmail.com>
2023-08-20 21:22:13 +0800

    max_element and move range

---
+++ algorithm/max_element.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+namespace stdlib {
+template<typename InputIterator>
+InputIterator max_element(InputIterator first, InputIterator last) {
+  if (first == last) {
+    return first;
+  }
+
+  InputIterator largest = first;
+  while (++first != last) { // skip first
+    if (*largest < *first) {
+      largest = first;
+    }
+  }
+  return largest;
+}
+
+template<typename InputIterator, typename Compare>
+InputIterator max_element(InputIterator first, InputIterator last, Compare _compare) {
+  if (first == last) {
+    return first;
+  }
+
+  InputIterator largest = first;
+  while (++first != last) { // skip first
+    if (_compare(*largest, *first)) {
+      largest = first;
+    }
+  }
+  return largest;
+}
+}
+
+bool compare(int i, int j) { return i < j; }
+
+struct Functor {
+  bool operator()(int i, int j) { return i < j; }
+} functor;
+
+int main() {
+  int ints[] = {3, 7, 2, 5, 6, 4, 9};
+
+  // using default comparison:
+  std::cout << "The largest element is " << *stdlib::max_element(ints, ints + 7) << '\n';
+
+  // using compare compare as comp:
+  std::cout << "The largest element is " << *stdlib::max_element(ints, ints + 7, &compare) << '\n';
+
+  // using object functor as comp:
+  std::cout << "The largest element is " << *stdlib::max_element(ints, ints + 7, functor) << '\n';
+
+  return 0;
+}
---
+++ algorithm/move.cpp
@@ -0,0 +1,49 @@
+#include <utility>
+#include <vector>
+#include <string>
+#include <iostream>
+
+namespace stdlib {
+template<typename InputIterator, typename OutputIterator>
+OutputIterator move(InputIterator first, InputIterator last, OutputIterator out) {
+  while (first != last) {
+    *out = std::move(*first);
+    ++first;
+    ++out;
+  }
+  return out;
+}
+}
+
+int main() {
+  std::vector<std::string> foo = {"air", "water", "fire", "earth"};
+  std::vector<std::string> bar(4);
+
+  // moving ranges:
+  std::cout << "Moving ranges...\n";
+  stdlib::move(foo.begin(), foo.begin() + 4, bar.begin());
+
+  std::cout << "foo contains " << foo.size() << " elements:";
+  std::cout << " (each in an unspecified but valid state)";
+  std::cout << '\n';
+
+  std::cout << "bar contains " << bar.size() << " elements:";
+  for (std::string &x : bar) {
+    std::cout << " [" << x << "]";
+  }
+  std::cout << '\n';
+
+  // moving container:
+  std::cout << "Moving container...\n";
+  foo = std::move(bar);
+
+  std::cout << "foo contains " << foo.size() << " elements:";
+  for (std::string &x : foo) {
+    std::cout << " [" << x << "]";
+  }
+  std::cout << '\n';
+
+  std::cout << "bar is in an unspecified but valid state";
+  std::cout << '\n';
+  return 0;
+}
